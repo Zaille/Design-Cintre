6f5d88f15bef7fde2b8303ccaad9624f80a7f60b
etienne912 <etienne9912@gmail.com>
2019-03-25 22:52:57 +0100

    push index

--- js/script.js
+++ js/script.js
@@ -1,9 +1,16 @@
 $(() => {
 
-    $('.carousel').carousel();
+	$('.carousel').carousel();
 
-    $('div.div_search_icon').on('click', function () {
-        $('div.div_search_icon').toggleClass('div_search_icon_clicked');
-    });
+	$('div.div_search_icon').on('click', function () {
+		$('div.div_search_icon').toggleClass('div_search_icon_clicked');
+	});
 
+	$("#top_button").click(() => {
+		window.scroll({
+			top: 0,
+			left: 0,
+			behavior: 'smooth'
+		});
+	});
 });
