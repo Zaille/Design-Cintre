b673f3dfc1ac83fc9c79d3a6e8d104cd5504b15f
Zaille <lucas.hervouet@etu.univ-nantes.fr>
2019-03-25 19:15:01 +0100

    Upadate Header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-$('.carousel').carousel();
+$(() => {
+
+    $('.carousel').carousel();
+
+    $('div.div_search_icon').on('click', function () {
+        $('div.div_search_icon').toggleClass('div_search_icon_clicked');
+    });
+
+});
